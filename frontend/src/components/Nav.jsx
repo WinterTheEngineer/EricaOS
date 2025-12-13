06aeaf99773b55d631ef987edfe87d9f7544f84b
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2025-12-14 01:46:44 +0200

    added responsive navigation component

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,59 @@
+import '../styles/Nav.css';
+import { useState, useEffect } from "react";
+import logo from '../assets/sidebar-logo.png';
+import { RiMenu3Fill } from "react-icons/ri";
+
+function Nav () {
+
+    const toggleMobileMenu = () => {
+        const mobileMenu = document.getElementById('nav');
+        const subMenu = document.querySelector('.nav-list.mobile')
+        
+        mobileMenu.classList.toggle('active');
+
+        if (!subMenu.classList.contains('active')) {
+            setTimeout(() => {
+                subMenu.classList.toggle('active');
+            }, 600)
+        } else {
+            subMenu.classList.toggle('active');
+        }
+
+
+    }
+
+    return (
+        <nav id='nav'>
+            <div className="nav-container">
+                <img className="logo" src={logo} alt="The official logo for EricaOS" />
+                <ul className="nav-list desktop">
+                    <li>Home</li>
+                    <li>Features</li>
+                    <li>Pricing</li>
+                    <li>Developers</li>
+                    <li>Contact</li>
+                </ul>
+                <div className="nav-actions">
+                    <a href="http://" className='sign-in'>Sign in</a>
+                    <a href="http://" className='sign-up'>Sign Up</a>
+                </div>
+                <button
+                    id='nav-toggle'
+                    className='mobile'
+                    onClick={toggleMobileMenu}
+                >
+                    <RiMenu3Fill />
+                </button>
+            </div>
+            <ul className="nav-list mobile">
+                <li>Home</li>
+                <li>Features</li>
+                <li>Pricing</li>
+                <li>Developers</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
