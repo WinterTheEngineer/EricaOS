ac6408316cad145cb588bbeed9bba644ab95c769
Winter The Engineer <rhulanimogotsi@gmail.com>
2026-06-24 16:40:40 +0200

    import fixes #2

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import '../styles/form.css'
+import '../styles/Form.css'
 import { login } from '../utils/authService';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'
