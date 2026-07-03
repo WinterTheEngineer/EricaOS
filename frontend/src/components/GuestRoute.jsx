45aacae1cd14ef56abbf894bbf78f779d141ef4d
Winter The Engineer <rhulanimogotsi@gmail.com>
2026-07-03 12:08:48 +0200

    fixing GuestRoute bug

--- frontend/src/components/GuestRoute.jsx
+++ frontend/src/components/GuestRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom"
 import { jwtDecode } from "jwt-decode"
-import { ACCESS_TOKEN } from "../constants"
+import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants"
 
 function GuestRoute({ children }) {
     const token = localStorage.getItem(ACCESS_TOKEN)
@@ -15,14 +15,18 @@ function GuestRoute({ children }) {
         const now = Date.now() / 1000
 
         if (tokenExpiration < now) {
+            localStorage.removeItem(ACCESS_TOKEN)
+            localStorage.removeItem(REFRESH_TOKEN)
             return children
         }
     } catch {
+        localStorage.removeItem(ACCESS_TOKEN);
+        localStorage.removeItem(REFRESH_TOKEN);
         return children
     }
 
     // Otherwise → user is already logged in → redirect to home
-    return <Navigate to="/" />
+    return <Navigate to="/dashboard" />
 }
 
 export default GuestRoute
